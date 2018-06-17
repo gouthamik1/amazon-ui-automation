9762681eba22b17caee2fd024f9be484f2f3b5d9
gouthamik <gouthami.k1@gmail.com>
2018-06-16 18:03:53 -0700

    Selenium Java UI automation using Page Objects : Logs in to Amazon, searches a product, adds to cart, and then finally cleans up, and logs out.

---
+++ Amazon_SearchProduct__AddToShoppingCart_CleanupCart/src/test/java/com/kammari/config/AppConfiguration.java
@@ -0,0 +1,14 @@
+package com.kammari.config;
+
+
+
+
+public class AppConfiguration extends Configuration {
+    public static String BaseUrl = "https://www.amazon.com/";
+    public static String Email = "<Your Amazon Email Here>";
+    public static String Password = "<Your Amazon Password Here>";
+
+    public static String ProductPartialName = "My First ABC (My First Books)";
+
+
+}
